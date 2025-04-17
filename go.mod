a499a5d79ff018fdd9cb9565eb731ce5866b3ed8
Nikita Galushko <galnikrom@gmail.com>
2025-04-17 12:47:53 +0300

    support third party swisstable implementations to benchmark

--- go.mod
+++ go.mod
@@ -2,4 +2,11 @@ module github.com/nikgalushko/swisstable-bench
 
 go 1.24.0
 
-require pgregory.net/rand v1.0.2 // indirect
+require (
+	github.com/cockroachdb/swiss v0.0.0-20250327203710-2932b022f6df
+	github.com/crn4/swiss v0.0.0-20241005224259-45fab3684456
+	github.com/dolthub/swiss v0.2.1
+	pgregory.net/rand v1.0.2
+)
+
+require github.com/dolthub/maphash v0.1.0 // indirect
--- main.go
+++ main.go
@@ -3,20 +3,35 @@ package main
 import (
 	"flag"
 	"fmt"
-)
 
-const (
-	numElements = 1000000
-	numWorkers  = 10
+	cocroach "github.com/cockroachdb/swiss"
+	crn4 "github.com/crn4/swiss"
+	dolthub "github.com/dolthub/swiss"
 )
 
 func main() {
-	var seed, size uint64
+	var (
+		seed, size         uint64
+		mapType            string
+		keyType, valueType string
+	)
 	flag.Uint64Var(&seed, "seed", 1234, "Seed value for random generator")
 	flag.Uint64Var(&size, "dataset-size", 1_000_000, "Number of elements in the dataset")
+	flag.StringVar(&mapType, "map-type", "std", "std/cocroach/crn4/dolthub")
+	flag.StringVar(&keyType, "key-type", "int", "int/string/struct{}")
+	flag.StringVar(&valueType, "value-type", "int", "int/string/struct{}")
 	flag.Parse()
 
-	b := New[int, int](size, seed, func() Map[int, int] { return NewSimpleMap[int, int]() })
+	build := func() Map[int, int] { return NewSimpleMap[int, int]() }
+	switch mapType {
+	case "cocroach":
+		build = func() Map[int, int] { return NewCocroachMap[int, int]() }
+	case "crn4":
+		build = func() Map[int, int] { return NewCRN4Map[int, int]() }
+	case "dolthub":
+		build = func() Map[int, int] { return NewDolthubMap[int, int]() }
+	}
+	b := New[int, int](size, seed, build)
 
 	fmt.Println("Running Map Benchmarks")
 
@@ -42,4 +57,67 @@ func (m *SimpleMap[K, V]) Set(key K, value V) {
 
 func (m *SimpleMap[K, V]) Delete(key K) {
 	delete(m.data, key)
+}
+
+type Cocroach[K comparable, V any] struct {
+	data *cocroach.Map[K, V]
+}
+
+func NewCocroachMap[K comparable, V any]() *Cocroach[K, V] {
+	return &Cocroach[K, V]{data: cocroach.New[K, V](0)}
+}
+
+func (m *Cocroach[K, V]) Get(key K) (V, bool) {
+	value, ok := m.data.Get(key)
+	return value, ok
+}
+
+func (m *Cocroach[K, V]) Set(key K, value V) {
+	m.data.Put(key, value)
+}
+
+func (m *Cocroach[K, V]) Delete(key K) {
+	m.data.Delete(key)
+}
+
+type CRN4[K comparable, V any] struct {
+	data *crn4.Map[K, V]
+}
+
+func NewCRN4Map[K comparable, V any]() *CRN4[K, V] {
+	return &CRN4[K, V]{data: crn4.New[K, V](0)}
+}
+
+func (m *CRN4[K, V]) Get(key K) (V, bool) {
+	value, ok := m.data.Get(key)
+	return value, ok
+}
+
+func (m *CRN4[K, V]) Set(key K, value V) {
+	m.data.Put(key, value)
+}
+
+func (m *CRN4[K, V]) Delete(key K) {
+	m.data.Delete(key)
+}
+
+type Dolthub[K comparable, V any] struct {
+	data *dolthub.Map[K, V]
+}
+
+func NewDolthubMap[K comparable, V any]() *Dolthub[K, V] {
+	return &Dolthub[K, V]{data: dolthub.NewMap[K, V](0)}
+}
+
+func (m *Dolthub[K, V]) Get(key K) (V, bool) {
+	value, ok := m.data.Get(key)
+	return value, ok
+}
+
+func (m *Dolthub[K, V]) Set(key K, value V) {
+	m.data.Put(key, value)
+}
+
+func (m *Dolthub[K, V]) Delete(key K) {
+	m.data.Delete(key)
 }
